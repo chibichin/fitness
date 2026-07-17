0579a13098c561507e46a1ff3c8320a03792e498
chibichin <liangj11@my.smccd.edu>
2026-07-17 13:40:41 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="fitness-record-v1-options-menu";
+const CACHE="fitness-record-v1-1-0";
 const ASSETS=["./","index.html","css/app.css","js/app.js","js/storage.js","manifest.json"];
 self.addEventListener("install",event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))})
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))})
